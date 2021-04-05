bc3480823183242bb00fb99fd75990bab2f59269
hkjang <gagagiga@naver.com>
2021-04-06 08:00:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+node-red-contrib-ethermine
+================
+
+Node-RED node for ethermine
+
+
+## Install
+
+To install the stable version use the `Menu - Manage palette - Install`
+option and search for node-red-contrib-ethermine, or run the following
+command in your Node-RED user directory, typically `~/.node-red`
+
+    npm install node-red-contrib-ethermine
+
+## Wrapper Ethermine  API  
+- https://ethermine.org/api/
+
+## Sample parameters
+```js
+// msg.payload = {};
+// msg.payload.api = 'miner'; //pool or miner or worker
+// msg.payload.action = 'dashboard'; // if miner then action is history, poyouts, rounds, settings, currnetStats
+// msg.payload.api = 'pool'; //pool or miner or worker
+// msg.payload.action = 'poolStats'; // if pool then action is poolStats, blocksHistory, networkStats, serversHistory
+// msg.payload.api = 'worker'; //pool or miner or worker
+// msg.payload.action = 'workers'; // if pool then action is worker, history, currentStats, monitor
+
+return msg;
+```
+## Sample flows
+```json
+[{"id":"748930ca.60e81","type":"inject","z":"2422d0a1.5c053","name":"","props":[{"p":"payload"},{"p":"topic","vt":"str"}],"repeat":"","crontab":"","once":false,"onceDelay":0.1,"topic":"","payload":"","payloadType":"date","x":190,"y":80,"wires":[["8980037d.66ffb"]]},{"id":"8980037d.66ffb","type":"function","z":"2422d0a1.5c053","name":"","func":"// msg.payload = {};\n// msg.payload.api = 'miner'\n// msg.payload.action = 'dashboard'\nreturn msg;","outputs":1,"noerr":0,"initialize":"","finalize":"","x":380,"y":80,"wires":[["8186cef9.9d81"]]},{"id":"8186cef9.9d81","type":"ethermine","z":"2422d0a1.5c053","name":"","api":"miner","action":"currentStats","sort":"","start":"","creds":"7534ad3b.61b354","x":570,"y":80,"wires":[["53d0ea43.c95fc4"]]},{"id":"53d0ea43.c95fc4","type":"debug","z":"2422d0a1.5c053","name":"","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"false","statusVal":"","statusType":"auto","x":760,"y":80,"wires":[]},{"id":"7534ad3b.61b354","type":"ethermine-miner","name":""}]
+```
---
+++ node.js
@@ -0,0 +1,109 @@
+const axios = require("axios")
+module.exports = function (RED) {
+    function FunctionNode(n) {
+        RED.nodes.createNode(this, n);
+        if (RED.nodes.getNode(n.creds)){
+            this.miner = RED.nodes.getNode(n.creds).credentials.miner;
+            this.worker = RED.nodes.getNode(n.creds).credentials.worker;
+        } else {
+            this.miner = "";
+            this.worker = "";
+        }
+        var node = this;
+        this.name = n.name;
+
+        for (var key in n) {
+            node[key] = n[key] || "";
+        }
+        this.on('input', function (msg) {
+            for (var i in msg) {
+                if (i !== 'req' | i !== 'res' | i !== 'payload' | i !== 'send' | i !== '_msgid') {
+                    node[i] = node[i] || msg[i];
+                }
+            }
+            if(!node.url){
+                node.url = 'https://api.ethermine.org';
+            }
+
+            node.options = {};
+            // node.options.headers = {};
+            if(n.api && n.action){
+                node.payload = {};
+                node.payload.api = n.api;
+                node.payload.action = n.action;
+            }else{
+                if(!node.payload.api){
+                    msg.payload = 'need to msg.payload.api';
+                    node.send(msg);
+                }else if(!node.payload.action){
+                    msg.payload = 'need to msg.payload.action';
+                    node.send(msg);
+                }
+            }
+            if(node.payload.api.toLowerCase() === 'pool'){
+                if(node.payload.action === 'poolStats'){
+                    node.url += '/poolStats';
+                }else if(node.payload.action === 'blocksHistory'){
+                    node.url += '/blocks/history';
+                }else if(node.payload.action === 'networkStats'){
+                    node.url += '/networkStats';
+                }else if(node.payload.action === 'serversHistory'){
+                    node.url += '/servers/history';
+                }
+            }else if(node.payload.api.toLowerCase() === 'miner'){
+                if(node.payload.action === 'dashboard'){
+                    node.url += '/miner/' + node.miner + '/dashboard';
+                }else if(node.payload.action === 'history'){
+                    node.url += '/miner/' + node.miner + '/history';
+                }else if(node.payload.action === 'payouts'){
+                    node.url += '/miner/' + node.miner + '/payouts';
+                }else if(node.payload.action === 'rounds'){
+                    node.url += '/miner/' + node.miner + '/rounds';
+                }else if(node.payload.action === 'settings'){
+                    node.url += '/miner/' + node.miner + '/settings';
+                }else if(node.payload.action === 'currentStats'){
+                    node.url += '/miner/' + node.miner + '/currentStats';
+                }
+            }else if(node.payload.api.toLowerCase() === 'worker'){
+                if(node.payload.action === 'workers'){
+                    node.url += '/miner/' + node.miner + '/workers';
+                }else if(node.payload.action === 'history'){
+                    node.url += '/miner/' + node.miner + '/worker/' + node.worker + '/history';
+                }else if(node.payload.action === 'currentStats'){
+                    node.url += '/miner/' + node.miner + '/worker/' + node.worker +'/currentStats';
+                }else if(node.payload.action === 'monitor'){
+                    node.url += '/miner/' + node.miner + '/worker/' + node.worker +'/monitor';
+                }
+            }
+
+            axios.get(node.url, node.options)
+                .then(function (response){
+                    msg.payload = response.data;
+                    node.send(msg);
+                }).catch(function (err){
+                    msg.payload = err;
+                    node.send(msg);
+                });
+        });
+    }
+
+    RED.nodes.registerType("ethermine", FunctionNode, {
+        credentials: {
+            miner: {type:"text"},
+            worker: {type:"text"}
+        }
+    });
+
+    function ethermineMiner(n){
+        RED.nodes.createNode(this, n);
+        this.miner = n.miner;
+        this.worker = n.worker;
+    }
+
+    RED.nodes.registerType("ethermine-miner", ethermineMiner,{
+        credentials: {
+            miner: {type:"text"},
+            worker: {type:"text"}
+        }
+    });
+};
